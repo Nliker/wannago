2c03ccd7cdffda65269cbc6ced2c552efe71eb2a
서정현 <tjwjdgus83@naver.com>
2023-11-14 15:29:57 +0900

    application init

--- src/main/java/com/ssafy/wannago/WannagoApplication.java
+++ src/main/java/com/ssafy/wannago/WannagoApplication.java
@@ -2,12 +2,32 @@ package com.ssafy.wannago;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
+@Configuration
+@ComponentScan(basePackages = {"com.example"})
 public class WannagoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WannagoApplication.class, args);
 	}
-
+	
+	@Bean
+	public CharacterEncodingFilter characterEncodingFilter() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return characterEncodingFilter;
+	}
+	
+	@Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
