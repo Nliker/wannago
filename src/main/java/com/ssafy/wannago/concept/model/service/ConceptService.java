bcca506702ade0efa791e08a73eea4fdf0803576
서정현 <tjwjdgus83@naver.com>
2023-11-15 17:54:34 +0900

    add method

--- src/main/java/com/ssafy/wannago/concept/model/service/ConceptService.java
+++ src/main/java/com/ssafy/wannago/concept/model/service/ConceptService.java
@@ -2,12 +2,14 @@ package com.ssafy.wannago.concept.model.service;
 
 import java.util.List;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.ssafy.wannago.concept.model.ConceptDetailResponseDto;
 import com.ssafy.wannago.concept.model.ConceptDto;
 import com.ssafy.wannago.concept.model.ConceptResponseDto;
 
 public interface ConceptService {
-	ConceptResponseDto createConcept(String userId, ConceptDto concept) throws Exception;
+	ConceptResponseDto createConcept(String userId, ConceptDto concept, MultipartFile[] medias) throws Exception;
 
 	List<ConceptResponseDto> getConceptList(String userId) throws Exception;
 
