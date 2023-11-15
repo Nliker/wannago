2e4817db46a331f32927a60a6cb07e98bf540872
서정현 <tjwjdgus83@naver.com>
2023-11-15 10:51:39 +0900

    delete unused method

--- src/main/java/com/ssafy/wannago/user/model/mapper/UserMapper.java
+++ src/main/java/com/ssafy/wannago/user/model/mapper/UserMapper.java
@@ -8,7 +8,6 @@ import com.ssafy.wannago.user.model.UserDto;
 
 @Mapper
 public interface UserMapper {
-	UserDto getLoginUser(String id,String password) throws SQLException ;
 	UserDto selectByUserId(String userId) throws SQLException;
 	UserDto selectByUserEmailId(String userEmailId);
 	void insertUser(UserDto user);
