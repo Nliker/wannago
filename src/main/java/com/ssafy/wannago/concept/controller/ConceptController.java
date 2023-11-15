4b409f442690621d105c1f9b878ac0fa720c3a4b
서정현 <tjwjdgus83@naver.com>
2023-11-15 15:59:35 +0900

    add getConcept

--- src/main/java/com/ssafy/wannago/concept/controller/ConceptController.java
+++ src/main/java/com/ssafy/wannago/concept/controller/ConceptController.java
@@ -1,15 +1,19 @@
 package com.ssafy.wannago.concept.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.wannago.concept.model.ConceptDetailResponseDto;
 import com.ssafy.wannago.concept.model.ConceptDto;
 import com.ssafy.wannago.concept.model.ConceptResponseDto;
 import com.ssafy.wannago.concept.model.service.ConceptService;
@@ -30,4 +34,19 @@ public class ConceptController {
 		result.put("conceptInfo",conceptService.createConcept(authentication.getName(),concept));
 		return ResponseEntity.ok().body(result);
 	}
+	
+	@GetMapping("")
+	public ResponseEntity<Map<String,List<ConceptResponseDto>>> concept(Authentication authentication) throws Exception{
+		Map<String,List<ConceptResponseDto>> result=new HashMap<>();
+		result.put("conceptInfoList",conceptService.getConceptList(authentication.getName()));
+		return ResponseEntity.ok().body(result);
+	}
+	
+	@GetMapping("/{conceptNo}")
+	public ResponseEntity<Map<String,ConceptDetailResponseDto>> concept(Authentication authentication,@PathVariable int conceptNo) throws Exception{
+		Map<String,ConceptDetailResponseDto> result=new HashMap<>();
+		result.put("conceptDetailInfo",conceptService.getConcept(authentication.getName(),conceptNo));
+		return ResponseEntity.ok().body(result);
+	}
+	
 }
