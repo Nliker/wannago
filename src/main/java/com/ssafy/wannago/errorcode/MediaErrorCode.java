b814c5bf2c72d22b65f2d9d5363c417ffc6a1d7f
서정현 <tjwjdgus83@naver.com>
2023-11-16 15:21:14 +0900

    add exceptions

---
+++ src/main/java/com/ssafy/wannago/errorcode/MediaErrorCode.java
@@ -0,0 +1,18 @@
+package com.ssafy.wannago.errorcode;
+
+import lombok.Getter;
+
+@Getter
+public enum MediaErrorCode {
+	NotFoundMedia(404,"해당 파일이 존재하지 않습니다."),
+	MediaUserIdNotMatch(401,"해당 파일의 소유주가 다릅니다.");
+	
+	
+	private int code;
+    private String description;
+    
+    private MediaErrorCode(int code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+}
