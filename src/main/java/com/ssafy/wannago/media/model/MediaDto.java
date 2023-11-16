bbc8ad8d857d15f417751eeaeda2750f818c87ba
서정현 <tjwjdgus83@naver.com>
2023-11-16 15:20:53 +0900

    add getPath method

--- src/main/java/com/ssafy/wannago/media/model/MediaDto.java
+++ src/main/java/com/ssafy/wannago/media/model/MediaDto.java
@@ -1,10 +1,14 @@
 package com.ssafy.wannago.media.model;
 
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Value;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
-public class MediaDto {
+public class MediaDto {	
 	private int mediaNo;
 	private int conceptNo;
 	private String mediaSaveFolder;
@@ -13,7 +17,24 @@ public class MediaDto {
 	private String mediaType;
 	
 	public String getLink() {
-		return "download"+this.mediaSaveFolder+"/"+this.mediaOriginFile+"/"+this.mediaSaveFile;
+		return "medias"+"/"+this.conceptNo;
+	}
+
+	public String getSavePath() {
+		return this.mediaSaveFolder+File.separator+this.mediaSaveFile;
+	}
+	public MediaDto(int conceptNo, String mediaSaveFolder, String mediaOriginFile, String mediaSaveFile,
+			String mediaType) {
+		super();
+		this.conceptNo = conceptNo;
+		this.mediaSaveFolder = mediaSaveFolder;
+		this.mediaOriginFile = mediaOriginFile;
+		this.mediaSaveFile = mediaSaveFile;
+		this.mediaType = mediaType;
+	}
+
+	public MediaDto() {
+		super();
 	}
 
 	@Override
