c8a4f81c80dd0c8f7e4756e357a31eb69660cf60
서정현 <tjwjdgus83@naver.com>
2023-11-17 11:14:01 +0900

    add bucketController

---
+++ src/main/java/com/ssafy/wannago/bucket/controller/BucketController.java
@@ -0,0 +1,15 @@
+package com.ssafy.wannago.bucket.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("buckets")
+@RequiredArgsConstructor
+public class BucketController {
+	
+}
