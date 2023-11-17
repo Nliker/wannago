a3fbbdfd8b7b70aef5c5d3b6d100742770d35437
서정현 <tjwjdgus83@naver.com>
2023-11-17 15:22:41 +0900

    add select method

--- src/main/java/com/ssafy/wannago/bucket/model/mapper/BucketMapper.java
+++ src/main/java/com/ssafy/wannago/bucket/model/mapper/BucketMapper.java
@@ -1,8 +1,12 @@
 package com.ssafy.wannago.bucket.model.mapper;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+
+import com.ssafy.wannago.bucket.model.BucketDto;
+import com.ssafy.wannago.bucket.model.BucketJoinAttractionDto;
 
 @Mapper
 public interface BucketMapper {
@@ -10,5 +14,9 @@ public interface BucketMapper {
 	int selectCntByConceptNo(int conceptNo) throws SQLException;
 
 	int selectDoneCntByConceptNo(int conceptNo) throws SQLException;
+
+	List<BucketJoinAttractionDto> selectByConceptNo(int conceptNo) throws SQLException;
+
+	void insertBucket(BucketDto bucket) throws SQLException;
 
 }
